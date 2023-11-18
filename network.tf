83b73fda80051c8dc33b969b7b26eba29216a707
tecnicocefet <tecnicocefet@gmail.com>
2023-11-18 16:44:17 -0300

    atualizando  arquivos

--- network.tf
+++ network.tf
@@ -55,7 +55,7 @@ resource "aws_security_group" "security_group" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["177.142.167.8/32"]
 
   }
 
